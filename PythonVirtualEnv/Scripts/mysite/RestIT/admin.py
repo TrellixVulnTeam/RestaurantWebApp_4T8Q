48c50565b90cc94667402b45f0bd86a759311d30
Seyhak Ly <seyhak.ly@gmail.com>
2019-10-06 18:17:46 +0200

    db

--- PythonVirtualEnv/Scripts/mysite/RestIT/admin.py
+++ PythonVirtualEnv/Scripts/mysite/RestIT/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Business, BusinessUser
 # Register your models here.
+
+admin.site.register(BusinessUser)
+admin.site.register(Business)
--- PythonVirtualEnv/Scripts/mysite/RestIT/models.py
+++ PythonVirtualEnv/Scripts/mysite/RestIT/models.py
@@ -7,19 +7,25 @@ class CommonUserInfo(models.Model):
     password = models.CharField(max_length=30)
     signInDate = models.DateTimeField(default=timezone.now())
     lastLoginDate = models.DateTimeField(auto_now=True)
-    avatar = models.ImageField()
+    avatar = models.ImageField(blank=True)
     email = models.EmailField()
-    address = models.TextField(max_length=100)
+    address = models.TextField(max_length=100,blank=True)
+    name = models.TextField(max_length=30,default="John")
+    surname = models.TextField(max_length=30,default="Kennedy")
+    def __str__(self):
+        return self.name + " " + self.surname
     
     class Meta:
         abstract = True
 
 class BusinessUser(CommonUserInfo):
-    ownerID = models.ManyToManyField("Business")
-    permissions = models.CharField(max_length=20)
+    ownerID = models.ManyToManyField("Business",blank=True)
+    permissions = models.CharField(max_length=20,blank=True)
 
 class Business(models.Model):
     owners = models.ManyToManyField("BusinessUser")
     businessName = models.CharField(max_length=40)
     address = models.TextField(max_length=100)
-    description = models.TextField(max_length=100)
+    description = models.TextField(max_length=100,blank=True)
+    def __str__(self):
+        return self.businessName
