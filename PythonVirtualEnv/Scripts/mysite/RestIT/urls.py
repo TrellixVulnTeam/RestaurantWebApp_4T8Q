e555e90a874d8d134e0fc2a663baf56f29e97f0d
Seyhak Ly <seyhak.ly@gmail.com>
2019-10-07 20:00:12 +0200

    l

--- PythonVirtualEnv/Scripts/mysite/RestIT/urls.py
+++ PythonVirtualEnv/Scripts/mysite/RestIT/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('product/all/', views.index, name='index'),
+     # ex: /polls/5/
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    # ex: /polls/5/results/
+    path('product/<int:review_id>/review/', views.reviews, name='reviews'),
+    # ex: /polls/5/add_review/
+    path('product/<int:product_id>/add_review/', views.add_review, name='add_review'),
 ]
--- PythonVirtualEnv/Scripts/mysite/RestIT/views.py
+++ PythonVirtualEnv/Scripts/mysite/RestIT/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-
+from django.template import loader
 # Create your views here.
 
 from django.http import HttpResponse
-
+from .models import Product
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #return HttpResponse("Hello, world. You're at the reviews index.")
+    productList = Product.objects.order_by('-name')[:5]
+    template = loader.get_template('index.html')
+    context = {
+        'productList': productList,
+    }
+    return HttpResponse(template.render(context, request))
+    #output = ', '.join([q.name for q in productList])
+    #return HttpResponse(output)
+
+def product_detail(request, product_id):
+    return HttpResponse("You're looking at product %s." % product_id)
+
+def reviews(request, review_id):
+    response = "You're looking at the results of review %s."
+    return HttpResponse(response % review_id)
+
+def add_review(request, review_id):
+    return HttpResponse("You're adding a review to product %s." % review_id)
