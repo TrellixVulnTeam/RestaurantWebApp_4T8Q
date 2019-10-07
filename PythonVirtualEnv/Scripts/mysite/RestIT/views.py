020fc4c0d0a51e314a959d51ce6ea2064ea68d2a
Seyhak Ly <seyhak.ly@gmail.com>
2019-10-07 20:58:51 +0200

    a

--- PythonVirtualEnv/Scripts/mysite/RestIT/views.py
+++ PythonVirtualEnv/Scripts/mysite/RestIT/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
+#from django.http import Http404
 # Create your views here.
 
 from django.http import HttpResponse
@@ -7,17 +8,27 @@ from .models import Product
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the reviews index.")
-    productList = Product.objects.order_by('-name')[:5]
+    '''productList = Product.objects.order_by('-name')[:5]
     template = loader.get_template('index.html')
     context = {
         'productList': productList,
     }
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(context, request))'''
+    productList = Product.objects.order_by('-name')[:5]
+    context = {'productList': productList}
+    return render(request, 'index.html', context)
     #output = ', '.join([q.name for q in productList])
     #return HttpResponse(output)
 
 def product_detail(request, product_id):
-    return HttpResponse("You're looking at product %s." % product_id)
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'product.html', {'product': product})
+    #try:
+       # product = Product.objects.get(pk=product_id)
+        #except Product.DoesNotExist:
+          #  raise Http404("Product does not exist")
+        #return render(request, 'product.html', {'Product': product})
+    #return HttpResponse("You're looking at product %s." % product_id)
 
 def reviews(request, review_id):
     response = "You're looking at the results of review %s."
