419820aa6f4fc18c2cc07323ebf4dc4f910af184
Seyhak Ly <seyhak.ly@gmail.com>
2019-10-07 12:55:31 +0200

    Finally working

--- PythonVirtualEnv/Scripts/mysite/RestIT/models.py
+++ PythonVirtualEnv/Scripts/mysite/RestIT/models.py
@@ -3,17 +3,17 @@ from django.utils import timezone
 
 # Create your models here.
 class CommonUserInfo(models.Model):
-    login = models.CharField(max_length=20)
-    password = models.CharField(max_length=30)
-    signInDate = models.DateTimeField(default=timezone.now())
+    login = models.CharField(max_length=20,blank=True)
+    password = models.CharField(max_length=30,blank=True)
+    signUpDate = models.DateTimeField(default=timezone.now())
     lastLoginDate = models.DateTimeField(auto_now=True)
     avatar = models.ImageField(blank=True)
-    email = models.EmailField()
+    email = models.EmailField(default="xyz@gmail.com")
     address = models.TextField(max_length=100,blank=True)
     name = models.TextField(max_length=30,default="John")
     surname = models.TextField(max_length=30,default="Kennedy")
     def __str__(self):
-        return self.name + " " + self.surname
+       return self.name + " " + self.surname
     
     class Meta:
         abstract = True
@@ -23,9 +23,9 @@ class BusinessUser(CommonUserInfo):
     permissions = models.CharField(max_length=20,blank=True)
 
 class Business(models.Model):
-    owners = models.ManyToManyField("BusinessUser")
-    businessName = models.CharField(max_length=40)
-    address = models.TextField(max_length=100)
+    owners = models.ManyToManyField("BusinessUser",blank=True)
+    businessName = models.CharField(default="My business",max_length=40)
+    address = models.TextField(max_length=100,blank=True)
     description = models.TextField(max_length=100,blank=True)
     def __str__(self):
         return self.businessName
