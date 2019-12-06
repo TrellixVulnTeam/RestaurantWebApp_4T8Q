f16d4635223f7843faf80b138de46fa1ef1f9521
Seyhak Ly <seyhak.ly@gmail.com>
2019-12-06 20:13:04 +0100

    Remodelling html structure to modules

---
+++ PythonVirtualEnv/Scripts/RestIT/order_manager/static/components/order_reciever/order_receiver.js
@@ -0,0 +1,28 @@
+import OrdersBoard from './order_board/order_board';
+class OrdersReceiver extends React.Component{//waiting for extensions(? no idea what kind of yet)
+    constructor(props){
+      //console.log("props:");
+      //console.log(props);
+      super(props);
+      const orders = this.props.orders.results;
+      this.state = {
+        currentOrders:{orders},
+      }
+    }
+    render()
+    {
+      return(
+        <div className="order_board_container">
+          <OrdersBoard 
+            orders={this.state.currentOrders}
+            onOrderClick={id=>this.onOrderClick(id)}
+          />
+        </div>
+      );
+    }
+    onOrderClick(id){
+      alert(id);
+  
+    }
+  
+  }
